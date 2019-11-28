b9977e9a345f240adbd946c9a9ffd83fc442625d
Pelevina Anastasia <pelevina.2000@mail.ru>
2019-11-28 15:27:40 +0500

    task

--- emitter.js
+++ emitter.js
@@ -12,32 +12,75 @@ const isStar = true;
  */
 function getEmitter() {
     return {
+        events: {},
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!Object.keys(this.events).includes(event)) {
+                this.events[event] = [];
+            }
+            const contextEvent = {
+                context: context,
+                handler: handler
+            };
+            this.events[event].push(contextEvent);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            const offEvents = Object.keys(this.events)
+                .filter(key => (key.startsWith(`${event}.`) || key === event));
+            for (const offEvent of offEvents) {
+                const eventIndex = this.events[offEvent].findIndex(e => e.context === context);
+                this.events[offEvent].splice(eventIndex, 1);
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            const eventSplit = event.split('.');
+            const events = [eventSplit[0]];
+            for (let i = 1; i < eventSplit.length; i++) {
+                events.push(`${events[i - 1]}.${eventSplit[i]}`);
+            }
+            events.reverse();
+
+            for (const e of events) {
+                if (!Object.keys(this.events).includes(e)) {
+                    continue;
+                }
+                const eventsToEmit = this.events[e];
+                eventsToEmit.forEach(eventEmit => {
+                    if (eventEmit.times) {
+                        checkSeveral(eventEmit);
+                    } else if (eventEmit.frequency) {
+                        checkThrough(eventEmit);
+                    } else {
+                        eventEmit.handler.call(eventEmit.context);
+                    }
+                });
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +90,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            this.on(event, context, handler);
+            const action = this.events[event].find(e => e.context === context);
+            action.times = times;
+            action.emitTimes = 0;
+
+            return this;
         },
 
         /**
@@ -59,11 +108,29 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            this.on(event, context, handler);
+            const action = this.events[event].find(e => e.context === context);
+            action.frequency = frequency;
+            action.emitTimes = 0;
+
+            return this;
         }
     };
+}
+
+function checkSeveral(eventEmit) {
+    if (eventEmit.times > eventEmit.emitTimes++) {
+        eventEmit.handler.call(eventEmit.context);
+    }
+}
+
+function checkThrough(eventEmit) {
+    if (eventEmit.emitTimes++ % eventEmit.frequency === 0) {
+        eventEmit.handler.call(eventEmit.context);
+    }
 }
 
 module.exports = {
